65116de09393c8cefb44ddea48886919531a32f6
Irshad Ahmed <Irshadahmed31@outlook.com>
2018-09-26 08:14:12 -0700

    Add project files.

---
+++ PostSharpBasics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using PostSharp.Aspects;
+
+namespace PostSharpBasics
+{
+    [Serializable]
+    public class MethodStartAndStop : OnMethodBoundaryAspect
+    {
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            Console.WriteLine("The {0} method has been entered.", args.Method.Name);
+        }
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            Console.WriteLine("The {0} method has exited.", args.Method.Name);
+            Console.ReadKey();
+        }
+    }
+
+    static class Program
+    {
+        [MethodStartAndStop]
+        static void Main()
+        {
+        }
+    }
+}
